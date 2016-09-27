046e43065577bc0a996cf6da65d0e0cc79d52b8b
Samuil Gospodinov <samuil4@gmail.com>
2016-09-27 18:24:14 +0300

    Project base setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,111 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        
+
+        watch: {
+
+            html: {
+                files: ['src/*.html'],
+                tasks: ['copy:html']
+            }
+        },
+
+        copy: {
+
+            dev: {
+                files: [
+                    {expand: true, cwd: 'src', src: ['index.html'], dest: 'dev/'},
+                    {
+                        expand: true,
+                        cwd: './bower_components/materialize/dist/fonts',
+                        src: ['**'],
+                        dest: 'dev/fonts'
+                    },
+                    {
+                        expand: true,
+                        cwd: './bower_components/materialize/dist/css',
+                        src: ['**'],
+                        dest: 'dev/css'
+                    },
+                    {expand: true, cwd: 'src/img', src: ['**'], dest: 'dev/img'}
+                ]
+            },
+
+            html: {
+                files: [
+                    {expand: true, cwd: 'src', src: ['index.html'], dest: 'dev/'}
+                ]
+            }
+        },
+
+        clean: {
+            dev: {
+                src: ['dev']
+            }
+        },
+
+        browserify: {
+
+            options: {
+
+                transform: [
+                    ['stringify', {
+                        appliesTo: { includeExtensions: ['.html']}
+                    }],
+                    ['babelify', { 'presets': ['es2015'], 'compact' : true }]
+                ]
+            },
+
+
+            vendor: {
+                src: [],
+                dest: 'dev/vendor.js',
+                options: {
+                    alias: [
+                        './bower_components/angular/angular.min.js:angular',
+                        './bower_components/angular-ui-router/release/angular-ui-router.min.js:angular-ui-router',
+                        './src/vendor/underscore.js:underscore'
+                    ]
+                }
+            },
+
+            watchClient: {
+
+                files: {
+                    'dev/client.js': ['src/app/app.module.js']
+                },
+
+                options: {
+
+                    browserifyOptions: {
+                        debug: true
+                    },
+
+                    watch: true,
+                    external: [
+                        'angular',
+                        'ui-router',
+                        'underscore'
+                    ],
+
+                    alias: {}
+                }
+            }
+        }
+
     });
+
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', [
+        'clean:dev', 
+        'browserify:vendor',
+        'browserify:watchClient', 
+        'copy:dev', 
+        'watch'
+    ]);
+
 };
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # angular-course-1.5
-Course materials
+
+
+## Technologies
+- ES6 ES2015
+- Grunt
+- Angular 1.5.x
+- Browserify
+
+**Styleguide**
+  https://github.com/johnpapa/angular-styleguide/blob/master/a1/README.md
+
+## Installation
+
+####1. Clone this project
+
+
+```sh
+$ git clone https://github.com/CodiCamp/angular-course-1.5
+```
+
+####2.  Make sure you have [bower](http://bower.io/), [grunt-cli](https://www.npmjs.com/package/grunt-cli) and  [npm](https://www.npmjs.org/) installed globally
+
+```sh
+$ npm install -g grunt-cli
+$ npm install -g bower
+```
+
+####3. On the command prompt run the following commands
+
+```sh
+$ cd `project-directory`
+```
+
+```sh
+$ npm install
+```
+
+```sh
+$ bower install
+```
